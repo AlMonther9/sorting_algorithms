0247618e490f48bea29728642464b35e2a51c7cf
AlMonther9 <139255318+AlMonther9@users.noreply.github.com>
2024-01-28 13:01:11 +0000

    added quick sort function

--- sort.h
+++ sort.h
@@ -1,9 +1,20 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+/*Comparison direction macros for bitonic sort*/
+#define UP 0
+#define DOWN 1
+
+/*enum bool - Enumeration of boolean values*/
+typedef enum bool
+{
+    false = 0,
+    true
+} bool;
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,11 +30,19 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
+/*Helper for swapping*/
 void swap_ints(int *a, int *b);
+
+/*Printing helper functions */
+void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/*Regular functions prototypes*/
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+
+
+#endif
