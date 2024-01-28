04bdc503c151c7168ba46c87842e3fd617cde067
AlMonther9 <139255318+AlMonther9@users.noreply.github.com>
2024-01-28 12:45:22 +0000

    just created the selecttion sort function and tested it, works

--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ void print_array(const int *array, size_t size);
 void swap_ints(int *a, int *b);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
